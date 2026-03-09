4dc7ad1f4c9ba1ef86c6e9e9e2d1752724c6c164
swathi bawge <swathiveda4632@gmail.com>
2026-03-09 22:23:24 +0530

    Update none.py

--- keywords/none.py
+++ keywords/none.py
@@ -1,1 +1,23 @@
+x = None
+if x is None:
+  print("x has no value")
+else:
+  print(x)
+"""
+output:
+x has no value
+"""
+#----------------------------------------------------
+"""
+#---------------example in functions-----------------
+def swathi():
+  print("hello")
+result = swathi()
+print(result)
+
+#output
+#hello
+#None
+"""
+
 
