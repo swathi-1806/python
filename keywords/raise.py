875fde0b7bf17758ce2218433e7bf3e616ba0ae7
swathi bawge <swathiveda4632@gmail.com>
2026-03-10 21:01:29 +0530

    Update raise.py

--- keywords/raise.py
+++ keywords/raise.py
@@ -3,3 +3,12 @@ if age < 0:
     raise ValueError("invalid age: ")
 else:
     print("your age is", age)
+
+"""
+enter your age:21
+your age is 21
+---------------------
+enter your age:-5
+ValueError: invalid age:
+"""
+
