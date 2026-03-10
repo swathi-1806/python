bf910ff5b17556df948fb57efdbcd8ae298f69f4
swathi bawge <swathiveda4632@gmail.com>
2026-03-10 20:54:30 +0530

    Update except.py

--- keywords/except.py
+++ keywords/except.py
@@ -1,1 +1,22 @@
+try:
+  x = int(input("enter the value:"))
+  y=10/x
+except zeroDivisionError:
+  print("division by zero error")
+except valueError:
+  print("invalid input")
+finally:
+  print("programm completed")
 
+"""
+Enter number: 5
+Program finished
+-----------------------
+Enter number: 0
+Division by zero error
+Program finished
+----------------------
+Enter number: x
+Invalid input
+Program finished
+"""
