e5959eec94d70b59fdb9aed2586b3fdbcc769ee8
swathi bawge <swathiveda4632@gmail.com>
2026-03-11 20:22:34 +0530

    Create continue.py

---
+++ keywords/continue.py
@@ -0,0 +1,21 @@
+print("odd numbers between 0 and 20 are:")
+for i in range(0,20):
+  if i%2==0:
+    continue
+  
+  print(i)
+  
+"""
+output:-
+odd numbers between 0 and 20 are:
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+  """
