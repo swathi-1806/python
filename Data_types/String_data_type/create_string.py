60dd62ec4fdca0c8183103af6a014245fe80fabe
swathi bawge <swathiveda4632@gmail.com>
2026-03-12 22:29:23 +0530

    Create create_string.py

---
+++ Data_types/String_data_type/create_string.py
@@ -0,0 +1,13 @@
+print("---usg_single_quotes---")
+name='swathi'
+print(name)
+
+print("---usg_double_quotes---")
+text="it's beautiful day"
+print(text)
+
+print("---usg_triple_quotes---")
+message="""python is easy
+python is powerfull
+python is popular"""
+print(message)
