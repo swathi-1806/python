9d596644c8c6e0ae4e48dd6b469dec9817e82b64
swathi bawge <swathiveda4632@gmail.com>
2026-03-15 20:08:41 +0530

    Update create_list.py

--- Data_types/sequence/list/create_list.py
+++ Data_types/sequence/list/create_list.py
@@ -1,1 +1,9 @@
+vegetables=["beans","carrot","potato","brinjal","onion"]
+print(vegetables)
+print(type(vegetables))
 
+"""
+output:
+['beans', 'carrot', 'potato', 'brinjal', 'onion']
+<class 'list'>
+"""
