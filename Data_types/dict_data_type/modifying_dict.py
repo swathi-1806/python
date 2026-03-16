bae710bb116bcb4c7d2b0e0ad476a1a33953f50c
swathi bawge <swathiveda4632@gmail.com>
2026-03-16 20:17:20 +0530

    Add files via upload

---
+++ Data_types/dict_data_type/modifying_dict.py
@@ -0,0 +1,13 @@
+student={
+"name":"swathi",
+"age" :21,
+"branch":"ece"
+}
+student["age"]=22
+print(student)
+print(len(student))
+
+"""
+output:
+{'name': 'swathi', 'age': 22, 'branch': 'ece'}
+"""
