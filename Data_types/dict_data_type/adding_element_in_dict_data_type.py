3c5e5488d4f9fb67568a9a9dae009287b79984b5
swathi bawge <swathiveda4632@gmail.com>
2026-03-16 20:16:32 +0530

    Add files via upload

---
+++ Data_types/dict_data_type/adding_element_in_dict_data_type.py
@@ -0,0 +1,12 @@
+student={
+"name":"swathi",
+"age" :21,
+"branch":"ece"
+}
+student["college"]="ABC college"
+print(student)
+
+"""
+output:
+{'name': 'swathi', 'age': 21, 'branch': 'ece', 'college': 'ABC college'}
+"""
