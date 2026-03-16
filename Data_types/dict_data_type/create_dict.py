85cb25857d9e30dce4b561f5f8be59399a793416
swathi bawge <swathiveda4632@gmail.com>
2026-03-16 20:17:00 +0530

    Add files via upload

---
+++ Data_types/dict_data_type/create_dict.py
@@ -0,0 +1,13 @@
+student ={
+        "name":"swathi",
+        "age":21,
+        "branch":"ece"
+}
+print(student)
+print(type(student))
+
+"""
+output:
+{'name': 'swathi', 'age': 21, 'branch': 'ece'}
+<class 'dict'>
+"""
