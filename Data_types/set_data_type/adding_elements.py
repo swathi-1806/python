8c0b5dc4dbdc842a1355a7eb613a2104646e7406
swathi bawge <swathiveda4632@gmail.com>
2026-03-17 20:23:21 +0530

    Add files via upload

---
+++ Data_types/set_data_type/adding_elements.py
@@ -0,0 +1,8 @@
+fruits={"apple","banana","mango"}
+fruits.add("litchi")
+print(fruits)
+
+"""
+output
+{'apple', 'litchi', 'banana', 'mango'}
+"""
---
+++ Data_types/set_data_type/removing_elements.py
@@ -0,0 +1,11 @@
+fruits={"apple","banana","mango","litchi"}
+fruits.remove("banana")
+print(fruits)
+print(len(fruits))
+
+"""
+output
+{'litchi', 'mango', 'apple'}
+3
+
+"""
