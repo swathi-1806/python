3503b9f58121581a67c8e343daf24e1434f6e8dc
swathi bawge <swathiveda4632@gmail.com>
2026-03-18 21:40:04 +0530

    Update ascii_to_char.py

--- ASCII/ascii_to_char.py
+++ ASCII/ascii_to_char.py
@@ -1,6 +1,12 @@
 ascii_value=65
-ascii_value1=95
-char_of_65=char(ascii_value)
-char_of_95=char(ascii_value1)
+ascii_value1=75
+char_of_65=chr(ascii_value)
+char_of_75=chr(ascii_value1)
 print("char for ascii value 65 is:",char_of_65)
-print("char for ascii value 95 is:",char_of_95)
+print("char for ascii value 95 is:",char_of_75)
+
+"""
+output
+char for ascii value 65 is: A
+char for ascii value 95 is: K
+"""
