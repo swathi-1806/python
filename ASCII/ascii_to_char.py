44c4b787951448170aad4e7bd1ed27b13c9bb988
swathi bawge <swathiveda4632@gmail.com>
2026-03-18 21:26:46 +0530

    Create ascii_to_char.py

---
+++ ASCII/ascii_to_char.py
@@ -0,0 +1,6 @@
+ascii_value=65
+ascii_value1=95
+char_of_65=char(ascii_value)
+char_of_95=char(ascii_value1)
+print("char for ascii value 65 is:",char_of_65)
+print("char for ascii value 95 is:",char_of_95)
