9df0c75539aaf552406aba444e4be96f7987c806
swathi bawge <swathiveda4632@gmail.com>
2026-03-19 20:17:56 +0530

    Update membership_operator.py

--- operators/membership_operator.py
+++ operators/membership_operator.py
@@ -1,1 +1,7 @@
+t=[1,2,3,4,5,6]
+print("is 1 is in the list?:",1 in t)
+print("is 10 is in the list?:",10 in t)
+print("is 2 is not in the list?:",1 not in t)
+print("is 12 is not in the list?:",1 not in t)
+
 
