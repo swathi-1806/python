484b9dc3341df80512a7a795683e07948ea4b4da
swathi bawge <swathiveda4632@gmail.com>
2026-03-19 20:10:35 +0530

    Update logical_operator.py

--- operators/logical_operator.py
+++ operators/logical_operator.py
@@ -1,1 +1,14 @@
+x=true
+y=false
 
+#and
+result_and=x and y
+print("and of (true and false):",result_and)
+
+#or
+result_or=x or y
+print("or of (true or false):",result_or)
+
+#not
+result_not=not x
+print("not of x(not true):",result_not)
