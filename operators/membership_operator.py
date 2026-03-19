50de1e9bfcc6856f58624a43f515050de4e4a95a
swathi bawge <swathiveda4632@gmail.com>
2026-03-19 20:18:42 +0530

    Update membership_operator.py

--- operators/membership_operator.py
+++ operators/membership_operator.py
@@ -1,7 +1,17 @@
 t=[1,2,3,4,5,6]
+
 print("is 1 is in the list?:",1 in t)
 print("is 10 is in the list?:",10 in t)
-print("is 2 is not in the list?:",1 not in t)
-print("is 12 is not in the list?:",1 not in t)
+print("is 2 is not in the list?:",2 not in t)
+print("is 12 is not in the list?:",12 not in t)
+
+
+"""
+output:
+is 1 is in the list?: True
+is 10 is in the list?: False
+is 2 is not in the list?: False
+is 12 is not in the list?: True
+"""
 
 
