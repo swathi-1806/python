eb35b40af5072df0e5e50aaf1b06aa7f5f8e6e68
swathi bawge <swathiveda4632@gmail.com>
2026-03-19 19:56:39 +0530

    Update comparision_operators.py

--- operators/comparision_operators.py
+++ operators/comparision_operators.py
@@ -1,1 +1,23 @@
+x=5
+y=10
+result_equal=x==y
+print("is x equal to y?",result_equal)
+
+result_notequal=x!=y
+print("is x equal to y?",result_notequal)
+
+result_greater=x>y
+print("is x greater to y?",result_greater)
+
+result_lesser=x<y
+print("is x lesser t0 y?",result_lesser)
+
+"""
+output:
+
+is x equal to y? False
+is x equal to y? True
+is x greater to y? False
+is x lesser t0 y? True
+"""
 
