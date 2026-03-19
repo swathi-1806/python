bc165cdc3faabf4cb7add62178aa9b2352f7f7db
swathi bawge <swathiveda4632@gmail.com>
2026-03-19 19:50:25 +0530

    Update assignment_operators.py

--- operators/assignment_operators.py
+++ operators/assignment_operators.py
@@ -1,1 +1,62 @@
+# assignment operatoors(==)
+x=10
+print("value of x:",x)
 
+#add and assign
+a=10
+a+=20
+print("value of a(10+20):",a)
+
+#sub and assign
+b=20
+b-=10
+print("value of b(20-10):",b)
+
+#multiply and assign
+c=10
+c*=20
+print("value of cafter mul with 20(10*20):",c)
+
+#divide and assign
+d=10
+d/=2
+print("value of d after divide (10/2):",d)
+
+h=10
+h/=3
+print("value of h after divide (10/3):",h)
+
+#floor divide and assign
+e=10
+e//=2
+print("value of e after floor divide and assign(10//2):",e)
+
+i=10
+i//=3
+print("value of i after floor divide and assign(10//3):",i)
+
+#modulo and assign
+f=10
+f%=2
+print("value of f after modulo and assign(10%2):",f)
+
+#exponentiate and assign
+g=2
+g**=3
+print("value of g after exponenetial(2**3):",g)
+
+"""
+output:
+
+value of x: 10
+value of a(10+20): 30
+value of b(20-10): 10
+value of cafter mul with 20(10*20): 200
+value of d after divide (10/2): 5.0
+value of h after divide (10/3): 3.3333333333333335
+value of e after floor divide and assign(10//2): 5
+value of i after floor divide and assign(10//3): 3
+value of f after modulo and assign(10%2): 0
+value of g after exponenetial(2**3): 8
+
+"""
