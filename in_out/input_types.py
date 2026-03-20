e5c57821473cc7309a334cf6f461d7c3dd41976e
swathi bawge <swathiveda4632@gmail.com>
2026-03-20 19:36:38 +0530

    Update input_types.py

--- in_out/input_types.py
+++ in_out/input_types.py
@@ -1,1 +1,28 @@
+#string input
+name=input("enter your name:")
+print("good morning",name)
+
+#integer input
+age=int(input("enter your  age:"))
+print(age)
+print(type(age))
+
+#float input
+price=float(input("enter the price:"))
+print(price)
+print(type(price))
+
+
+"""
+output:
+
+enter your name:swathi
+good morning swathi
+enter your  age:21
+21
+<class 'int'>
+enter the price:21.22
+21.22
+<class 'float'>
+"""
 
