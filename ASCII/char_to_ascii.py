d64b0de28cfc6be29ff0e76221d9d79028c3a2a2
swathi bawge <swathiveda4632@gmail.com>
2026-03-27 22:22:12 +0530

    Update char_to_ascii.py

--- ASCII/char_to_ascii.py
+++ ASCII/char_to_ascii.py
@@ -4,7 +4,9 @@ ascii_value_a=ord(char_a)
 ascii_value_b=ord(char_b)
 print("ASCII VALUE OF-A:{} AND ASCII_VALUE OF-B:{}".format(ascii_value_a,ascii_value_b))
 
+
 """
-output
+output:-
+
 ASCII VALUE OF-A:65 AND ASCII_VALUE OF-B:66
 """
