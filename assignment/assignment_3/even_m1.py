977f3158aa78db3aa8309d829f4746a6d0a5334d
swathi bawge <swathiveda4632@gmail.com>
2026-03-30 15:33:41 +0530

    Add files via upload

---
+++ assignment/assignment_3/even_m1.py
@@ -0,0 +1,9 @@
+n=int(input("n="))
+i=1
+
+while i<=10:
+    if(i%2==0):
+        print(i)
+        
+    i=i+1
+print("the code is executed successfully")  
---
+++ assignment/assignment_3/even_m2.py
@@ -0,0 +1,16 @@
+n=int(input("n:"))
+
+for i in range(1,n+1):
+    if(i%2==0):
+        print(i)
+print("the code is executed successfully")
+"""
+output:-
+n=10
+2
+4
+6
+8
+10
+
+"""
