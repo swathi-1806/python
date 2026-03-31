c4fe604e739bfc78d729f2b8c32e5dd7cc637c93
swathi bawge <swathiveda4632@gmail.com>
2026-03-31 18:46:01 +0530

    Add arithmetic operations for two input values

---
+++ assignment/assignment_1/arithemetic_operation.py
@@ -0,0 +1,10 @@
+x,y=input("enter a , b values:").split(",")
+a=int(x)
+b=int(y)
+print("addition:",a+b,",subtraction:",a-b,",multiplication:",a*b,",division",a/b,sep="")
+
+"""
+output:
+enter a , b values:100,50
+addition:150,subtraction:50,multiplication:5000,division2.0
+"""
