ed9d0d35946f1f0a5a0e2ed3f4f8cfd2b5333d58
swathi bawge <swathiveda4632@gmail.com>
2026-03-31 19:00:12 +0530

    Create area_of_circle.py

---
+++ assignment/assignment_1/area_of_circle.py
@@ -0,0 +1,9 @@
+r=int(input("enter the radius of the circle:"))
+area_of_circle=float(3.14*r*r)
+print(f"area of the circle ={area_of_circle}")
+
+"""
+output:-
+enter the radius of the circle:5
+area of the circle =78.5
+"""
