2c42c70ab2e0557d9a0313933b03966519708c07
swathi bawge <swathiveda4632@gmail.com>
2026-04-04 21:44:36 +0530

    Create example_2.py for weather decision making
    Add a simple weather decision-making script.

---
+++ decision_making/if_else/example_2.py
@@ -0,0 +1,15 @@
+w=input("is weather is sunny(yes/no):")
+if w.lower()=="yes":
+    print("go outside") 
+else:
+    print("stay at home")
+    
+
+"""
+output:
+is weather is sunny(yes/no):no
+stay at home
+------------------------------
+is weather is sunny(yes/no):yes
+go outside
+""" 
