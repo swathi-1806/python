2e4bfedb4dd958aec10c98b3afe010e46b00eb69
swathi bawge <swathiveda4632@gmail.com>
2026-04-04 21:43:53 +0530

    Update and rename example_3.py to example_1.py

---
+++ decision_making/if_else/example_1.py
@@ -0,0 +1,14 @@
+age=int(input("enter your age:"))
+if age>=18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")   
+
+"""
+output:
+enter your age:21
+you are eligible to vote
+-----------------------------
+enter your age:17
+you are not eligible to vote
+"""
