156bbc83067539fc7bf0805f2b2db833917e7f6f
swathi bawge <swathiveda4632@gmail.com>
2026-04-04 21:34:25 +0530

    Add files via upload

---
+++ decision_making/if/example_1.py
@@ -0,0 +1,9 @@
+age=int(input("enter your age:"))
+if age>=18:
+    print("you are eligible to vote")
+
+"""
+output:
+enter your age:21
+you are eligible to vote
+"""
---
+++ decision_making/if/example_2.py
@@ -0,0 +1,17 @@
+w = input("Is weather sunny? (yes/no): ")
+
+if w.lower() == "yes":
+    print("You can go outside and enjoy the weather")
+
+print("Code ends here")
+
+"""
+output:
+Is weather sunny? (yes/no): no
+Code ends here
+------------------------------
+Is weather sunny? (yes/no): yes
+You can go outside and enjoy the weather
+Code ends here
+
+"""
