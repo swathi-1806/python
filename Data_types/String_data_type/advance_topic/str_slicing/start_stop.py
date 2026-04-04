696ffd1a56f9478e8224e0b7d1f7998bf059db7b
swathi bawge <swathiveda4632@gmail.com>
2026-04-04 21:52:41 +0530

    Create start_stop.py

---
+++ Data_types/String_data_type/advance_topic/str_slicing/start_stop.py
@@ -0,0 +1,13 @@
+text=input("enter your message:")
+print(text[1:3])
+print(text[0:5])
+print(text[2:len(text)-1])
+
+"""
+output:
+enter your message:python programming
+yt
+pytho
+thon programmin
+
+"""
