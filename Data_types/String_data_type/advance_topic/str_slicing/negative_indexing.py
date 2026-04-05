08d233b4fe938aa5669b774d11e9eb6ec95bd5f6
swathi bawge <swathiveda4632@gmail.com>
2026-04-05 17:53:47 +0530

    Add files via upload

---
+++ Data_types/String_data_type/advance_topic/str_slicing/negative_indexing.py
@@ -0,0 +1,13 @@
+text="python"
+print(text[-1])
+print(text[-4:-1])
+print(text[:-2])
+print(text[-4:])
+
+"""
+output
+n
+tho
+pyth
+thon
+"""
---
+++ Data_types/String_data_type/advance_topic/str_slicing/only_stop.py
@@ -0,0 +1,9 @@
+text=input("enter your message:")
+print(text[:len(text)])
+print(text[:len(text)-5])
+
+"""
+output:
+python programming
+python progra
+"""
