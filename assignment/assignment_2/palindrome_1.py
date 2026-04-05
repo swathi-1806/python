d932e0496b195bddb5a6209fed502595627da8f5
swathi bawge <swathiveda4632@gmail.com>
2026-04-05 17:52:28 +0530

    Add files via upload

---
+++ assignment/assignment_2/palindrome_1.py
@@ -0,0 +1,14 @@
+str1=input("enter the input:")
+str2=str1[::-1]
+if str2==str1:
+    print("the string is palindrome")
+else:   
+    print("the string is not palindrome")
+print("the code is executed successfully")
+
+"""
+OUTPUT:
+enter the input:radar
+the string is palindrome
+the code is executed successfully
+"""
