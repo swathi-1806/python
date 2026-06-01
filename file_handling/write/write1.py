abc239491626d0a0518a074f958a1c59d0c0e820
swathi bawge <swathiveda4632@gmail.com>
2026-06-01 21:28:44 +0530

    Update write1.py

--- file_handling/write/write1.py
+++ file_handling/write/write1.py
@@ -1,1 +1,20 @@
+f = open("C:/Users/swath/Desktop/python/my_python/file_handling/t1.txt", "w")
 
+f.write(
+    "This is my first file handling program in python\n"
+    "I am learning how to read and write files in python\n"
+    "File handling is an important concept in programming\n"
+    "With file handling, we can create, read, update, and delete files in python\n"
+    "File handling is a powerful tool for data storage and manipulation in python"
+)
+
+f.close()
+
+"""
+the t1.txt consists of
+This is my first file handling program in python
+I am learning how to read and write files in python
+File handling is an important concept in programming
+With file handling, we can create, read, update, and delete files in python
+File handling is a powerful tool for data storage and manipulation in python
+"""
