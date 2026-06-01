b001bfee11e5450f17d099a1574b4852110cf0e8
swathi bawge <swathiveda4632@gmail.com>
2026-06-01 22:09:05 +0530

    Create read1.py

---
+++ file_handling/read/read1.py
@@ -0,0 +1,16 @@
+f= open("C:/Users/swath/Desktop/python/my_python/file_handling/t1.bin", "wb")
+f.write(b"101010\n"
+        b"111111\n"
+        b"101001\n")
+f.close()
+
+f= open("C:/Users/swath/Desktop/python/my_python/file_handling/t1.bin", "rb")    
+content = f.read()
+print(content)
+f.close()
+
+"""
+101010
+111111
+101001
+"""
