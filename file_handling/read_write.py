9a7a7fc5a4331e2b91683135f711b208c885269b
swathi bawge <swathiveda4632@gmail.com>
2026-06-01 22:52:10 +0530

    Add file handling for writing and reading content
    Implement file writing and reading in read_write.py

---
+++ file_handling/read_write.py
@@ -0,0 +1,17 @@
+f = open("C:/Users/swath/Desktop/python/my_python/file_handling/t1.txt", "w")
+
+f.write("welcome to india\n"
+        "india is a beautiful country\n"
+        "india is known for its rich culture and heritage\n"
+        "india is a land of diversity and unity\n"
+        "india is a country of festivals and celebrations\n" 
+)
+f.close()
+
+f = open("C:/Users/swath/Desktop/python/my_python/file_handling/t1.txt", "r")
+content = f.read()f = open("C:/Users/swath/Desktop/python/my_python/file_handling/t1.txt", "r")
+content = f.read()
+print(content)
+f.close()
+print(content)
+f.close()
